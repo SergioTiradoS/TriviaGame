f4feb71b3bf7580f80b2990034dcd3f610278397
Sergio <sergiotiradosantiago@gmail.com>
2018-05-22 18:50:00 -0500

    Release 1.11

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,116 +2,198 @@
 
     var vPreguntas = [
         {
-        "Pregunta":"¿Quién 'sabía que no sabía nada'?",
-        "Respuestas":{
+          "Pregunta":"¿Quién 'sabía que no sabía nada'?",
+          "Respuestas":{
             "a":"Aristóteles.",
             "b":"Ortega y Gasset.",
             "c":"Socrate."},
-        "Respuesta":"c",
-        "Explicación": "Sócrates sabía que no sabía nada.",
-        "Km":1
+          "Respuesta":"c",
+          "Explicación": "Sócrates sabía que no sabía nada.",
+          "Km":1
         },
         {
-        "Pregunta":"¿Qué es un 'ewok'?",
-        "Respuestas":{
+          "Pregunta":"¿Qué es un 'ewok'?",
+          "Respuestas":{
             "a":"Un libro en formato digital.",
             "b":"Un ser de 'La guerra de las Galaxias'.",
             "c":"El jefe de una nación india."},
-        "Respuesta":"b",
-        "Explicación": "Los 'ewok' habitan la luna boscosa en la saga de George Lucas.",
-        "Km":2
+          "Respuesta":"b",
+          "Explicación": "Los 'ewok' habitan la luna boscosa en la saga de George Lucas.",
+          "Km":2
         },
         {
-        "Pregunta":"¿En qué lugar del cuerpo se produce la insulina?",
-        "Respuestas":{
+          "Pregunta":"¿En qué lugar del cuerpo se produce la insulina?",
+          "Respuestas":{
             "a":"En la hipófisis.",
             "b":"En el páncreas.",
             "c":"En el duodeno."},
-        "Respuesta":"b",
-        "Explicación": "La insulina se produce en el páncreas.",
-        "Km":2
+          "Respuesta":"b",
+          "Explicación": "La insulina se produce en el páncreas.",
+          "Km":2
         },
         {
-        "Pregunta":"¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?",
-        "Respuestas":{
+          "Pregunta":"¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?",
+          "Respuestas":{
             "a":"Románico.",
             "b":"Gótico.",
             "c":"Barroco."},
-        "Respuesta":"b",
-        "Explicación": "Notre Dame es de estilo gótico.",
-        "Km":1
+          "Respuesta":"b",
+          "Explicación": "Notre Dame es de estilo gótico.",
+          "Km":1
         },
         {
-        "Pregunta":"¿Con qué se fabricaba el pergamino?",
-        "Respuestas":{
+          "Pregunta":"¿Con qué se fabricaba el pergamino?",
+          "Respuestas":{
             "a":"Con piel de animales.",
             "b":"Con tiras de madera.",
             "c":"Con hojas de arbusto."},
-        "Respuesta":"a",
-        "Explicación": "El pergamino se hacía con piel de res u otros animales.",
-        "Km":2
+          "Respuesta":"a",
+          "Explicación": "El pergamino se hacía con piel de res u otros animales.",
+          "Km":2
         },
         {
-        "Pregunta":"¿Quién 'sabía que no sabía nada'?",
-        "Respuestas":{
-            "a":"Aristóteles",
-            "b":"Ortega y Gasset",
-            "c":"Socrate"},
-        "Respuesta":"c",
-        "Explicación": "Sócrates sabía que no sabía nada.",
-        "Km":1
-        },
-        {
-        "Pregunta":"¿Qué es un dáctilo?",
-        "Respuestas":{
+          "Pregunta":"¿Qué es un dáctilo?",
+          "Respuestas":{
             "a":"Un paso de baile.",
             "b":"Un pie de métrica.",
             "c":"Un reptil."},
-        "Respuesta":"b",
-        "Explicación": "El dáctilo es un pie métrico compuesto por una sílaba larga seguida de dos breves.",
-        "Km":1
+          "Respuesta":"b",
+          "Explicación": "El dáctilo es un pie métrico compuesto por una sílaba larga seguida de dos breves.",
+          "Km":1
         },
         {
-        "Pregunta":"¿Quién fue famoso esposo de Frida Kahlo? ",
-        "Respuestas":{
+          "Pregunta":"¿Quién fue famoso esposo de Frida Kahlo? ",
+          "Respuestas":{
             "a":"José Vasconcelos.",
             "b":"Diego Rivera.",
             "c":"León Trotsky."},
-        "Respuesta":"b",
-        "Explicación": "Frida Kahlo se casó con el también pintor Diego Rivera.",
-        "Km":3
+          "Respuesta":"b",
+          "Explicación": "Frida Kahlo se casó con el también pintor Diego Rivera.",
+          "Km":3
         },
         {
-        "Pregunta":"¿Qué atleta se hizo famoso por utilizar una nueva técnica en atletismo?",
-        "Respuestas":{
+          "Pregunta":"¿Qué atleta se hizo famoso por utilizar una nueva técnica en atletismo?",
+          "Respuestas":{
             "a":"Dick Fosbury.",
             "b":"Carl Lewis.",
             "c":"Emil Zátopek."},
-        "Respuesta":"a",
-        "Explicación": "Dick Fosbury se hizo famoso al atacar de espaldas el listón en salto de altura, es el 'estilo Fosbury'.",
-        "Km":3
+          "Respuesta":"a",
+          "Explicación": "Dick Fosbury se hizo famoso al atacar de espaldas el listón en salto de altura, es el 'estilo Fosbury'.",
+          "Km":3
         },
         {
-        "Pregunta":"¿Qué país no tiene al euro como moneda? ",
-        "Respuestas":{
+          "Pregunta":"¿Qué país no tiene al euro como moneda? ",
+          "Respuestas":{
             "a":"Portugal.",
             "b":"Suiza.",
             "c":"Francia."},
-        "Respuesta":"b",
-        "Explicación": "Los suizos siguen fieles a su franco.",
-        "Km":1
+          "Respuesta":"b",
+          "Explicación": "Los suizos siguen fieles a su franco.",
+          "Km":1
         },
         {
-        "Pregunta":"¿Qué instrumento está expresamente 'bien afinado' en ciertas obras de Bach?",
-        "Respuestas":{
+          "Pregunta":"¿Qué instrumento está expresamente 'bien afinado' en ciertas obras de Bach?",
+          "Respuestas":{
             "a":"El clave.",
             "b":"El oboe.",
             "c":"Todos deben estar bien afinados para que suenen bien."},
-        "Respuesta":"a",
-        "Explicación": "'El clave bien temperado' está formado por preludios y fugas compuestos por Bach.",
-        "Km":3
-        } 
-    ];
+          "Respuesta":"a",
+          "Explicación": "'El clave bien temperado' está formado por preludios y fugas compuestos por Bach.",
+          "Km":3
+        },
+        {
+          "Pregunta":"¿Cuál es el grupo mayoritario dentro del islam?",
+          "Respuestas":{
+            "a":"Suníes.",
+            "b":"Chiíes.",
+            "c":"Alauíes."},
+          "Respuesta":"a",
+          "Explicación": "En el islam, los sunitas representan la mayoría. ",
+          "Km":2
+        },
+        {
+          "Pregunta":"¿En cuántos estados pueden aparecer los elementos? ",
+          "Respuestas":{
+            "a":"En dos.",
+            "b":"En tres.",
+            "c":"Al menos en cuatro."},
+          "Respuesta":"c",
+          "Explicación": "Los elementos pueden aparecer en estado sólido, líquido, gaseoso, plasmático y se investigan nuevas formas de agregación.",
+          "Km":3
+        },
+        {
+          "Pregunta":"¿Qué grasas hacen tan saludable el aceite de oliva?",
+          "Respuestas":{
+            "a":"Las saturadas.",
+            "b":"Las monoinsaturadas.",
+            "c":"Las trans."},
+          "Respuesta":"b",
+          "Explicación": "El ácido oleico contiene grasas monoinsaturadas que reducen el riesgo cardiovascular.",
+          "Km":2
+        },
+        {
+          "Pregunta":"¿Quién se defiende 'a la siciliana'?",
+          "Respuestas":{
+            "a":"Las negras.",
+            "b":"La Cosa Nostra.",
+            "c":"La selección italiana de fútbol."},
+          "Respuesta":"a",
+          "Explicación": "En ajedrez, la defensa siciliana la utilizan las negras.",
+          "Km":1
+        },
+        {
+          "Pregunta":"¿Qué era el Concorde?",
+          "Respuestas":{
+            "a":"Un tren bala",
+            "b":"Un transbordador espacial",
+            "c":"Un avión supersónico "},
+          "Respuesta":"c",
+          "Explicación": "Un avión supersónico de pasajeros que cubría rutas transatlánticas.",
+          "Km":1},
+        {
+          "Pregunta":"¿Qué era el Concorde?",
+          "Respuestas":{
+            "a":"Un tren bala",
+            "b":"Un transbordador espacial",
+            "c":"Un avión supersónico"},
+          "Respuesta":"c",
+          "Explicación": "Un avión supersónico de pasajeros que cubría rutas transatlánticas.",
+          "Km":1},
+        {"Pregunta":"Un uno seguido de cien ceros es también...","Respuestas":{"a":"El nombre de un buscador de Internet","b":"Una secuencia binaria de encriptación","c":"El nombre de un sistema solar"},"Respuesta":"a","Explicación": "El término gúgol (10 elevado a 100) es el origen del nombre de Google.","Km":1},
+        {"Pregunta":"¿Qué enfermedad se considera erradicada?","Respuestas":{"a":"Peste","b":"Viruela","c":"Cólera"},"Respuesta":"b","Explicación": "La viruela está erradicada.","Km":3},
+      {"Pregunta":"¿Qué le pasa en realidad al gato de Schrödinger?","Respuestas":{"a":"Que está muerto","b":"Que está vivo","c":"Que está muerto y vivo"},"Respuesta":"c","Explicación": "El gato de Schrödinger está vivo y muerto.","Km":1},
+      {"Pregunta":"¿Quién es el autor del Gaucho Martín Fierro?","Respuestas":{"a":"Martín Fierro","b":"José Hernández","c":"El autor es anónimo"},"Respuesta":"b","Explicación": "Martín Fierro es obra de José Hernández","Km":2},
+      {"Pregunta":"¿Quién es culpable?","Respuestas":{"a":"El imputado","b":"El acusado","c":"El condenado"},"Respuesta":"c","Explicación": "Culpable es el condenado por una sentencia judicial.","Km":3},
+      {"Pregunta":"¿Cuál es el primero de la lista de los números primos?","Respuestas":{"a":"El 1.","b":"El 2","c":"El 3"},"Respuesta":"b","Explicación": "El primer puesto de la lista de los números primos lo ocupa el 2.","Km":1},
+      {"Pregunta":"¿Cual es la moneda oficial de Panamá?","Respuestas":{"a":"El Dólar panameño","b":"El Dólar estadounidense","c":"El Balboa"},"Respuesta":"c","Explicación": "La moneda oficial es el Balboa","Km":1},
+      {"Pregunta":"¿A qué Ciudad-Estado pertenecía la localidad natal de Leonardo Da Vinci?","Respuestas":{"a":"A Venecia","b":"A Florencia","c":"A Milán"},"Respuesta":"b","Explicación": "Anchiano y Vinci eran parte de Florencia, en la Toscana italiana.","Km":2},
+      {"Pregunta":"¿Cuál es la nacionalidad del escritor y periodista Eduardo Galeano?","Respuestas":{"a":"Paraguayo","b":"Uruguayo","c":"Argentino"},"Respuesta":"b","Explicación": "Galeano nació en Montevideo, Uruguay.","Km":3},
+      {"Pregunta":"¿Qué veterano músico sigue 'llamando a las puertas del cielo'?","Respuestas":{"a":"Bob Dylan","b":"Miguel Ríos","c":"Herbert von Karajan"},"Respuesta":"a","Explicación": "Bob Dylan cantó 'Knockin' on Heaven's Door'.","Km":2},
+      {"Pregunta":"¿A quién le dice la Reina Roja que tiene que correr lo más rápido que pueda para quedarse en el mismo sitio?","Respuestas":{"a":"A Alicia","b":"A Pinocho","c":"A Pulgarcito"},"Respuesta":"a","Explicación": "La Reina Roja habla con Alicia en el País de las Maravillas.","Km":3},
+      {"Pregunta":"¿Cuál de las siguientes afirmaciones sobre los virus es incorrecta?","Respuestas":{"a":"Están vivos","b":"Se multiplican","c":"No tienen metabolismo"},"Respuesta":"a","Explicación": "Los virus no están vivos.","Km":2},
+      {"Pregunta":"¿Qué contiene más energía?","Respuestas":{"a":"100 gramos de espinacas","b":"100 gramos de nueces","c":"100 gramos de carne de ternera"},"Respuesta":"b","Explicación": "El mayor contenido de energía se encuentra en las nueces.","Km":3},
+      {"Pregunta":"¿Qué país no presenta reclamaciones territoriales sobre la Antártida?","Respuestas":{"a":"Francia","b":"Brasil","c":"Noruega"},"Respuesta":"b","Explicación": "Se trata de Brasil. Francia y Noruega forman parte del Tratado Antártico.","Km":1},
+      {"Pregunta":"¿Quién 'redescubrió' Machu Pichu para los europeos?","Respuestas":{"a":"Los conquistadores en el s.XVI","b":"Los colonos en el s. XVIII","c":"Prospecciones mineras en el s. XIX"},"Respuesta":"c","Explicación": "Las primeras noticias datan de finales del XIX y fueron ratificadas a principios del XX.","Km":1},
+      {"Pregunta":"La campana de Gauss está asociada a...","Respuestas":{"a":"La probabilidad","b":"La seguridad","c":"La imposibilidad"},"Respuesta":"a","Explicación": "La curva en campana de Gauss se utiliza en cálculos de probabilidades.","Km":1},
+      {"Pregunta":"¿Qué circula por un conductor?","Respuestas":{"a":"Radicales libres","b":"Fotones libres","c":"Electrones libres"},"Respuesta":"c","Explicación": "Por un conductor circulan electrones libres.","Km":1},
+      {"Pregunta":"¿En qué no cree un budista?","Respuestas":{"a":"En la reencarnación","b":"En el nirvana","c":"En un dios"},"Respuesta":"c","Explicación": "Los budistas no creen en ningún dios.","Km":3},
+      {"Pregunta":"¿Qué le sucede a la energía?","Respuestas":{"a":"Que se consume","b":"Que se transforma","c":"Que se destruye"},"Respuesta":"b","Explicación": "La energía ni se crea ni se destruye, solo se transforma.","Km":3},
+      {"Pregunta":"¿Quién fue Atahualpa Yupanqui?","Respuestas":{"a":"El último rey inca","b":"Un poeta argentino","c":"Pizarro, fue el sobrenombre dado por los incas"},"Respuesta":"b","Explicación": "Es el seudónimo quechua de Héctor Roberto Chavero Haram, gran poeta, escritor y cantautor argentino.","Km":3},
+      {"Pregunta":"¿Qué va antes?","Respuestas":{"a":"El plan de urbanización","b":"El permiso de construcción","c":"El plan de uso del suelo"},"Respuesta":"c","Explicación": "Lo primero es el plan de uso del suelo.","Km":2},
+      {"Pregunta":"El jamón ibérico no existiría sin...","Respuestas":{"a":"Níscalos y endrinas","b":"Encinas y alcornoques","c":"Pino piñonero y pino bellotero"},"Respuesta":"b","Explicación": "La dieta del cerdo ibérico incluye las bellotas de encinas y alcornoques.","Km":2},
+      {"Pregunta":"¿Qué papa convocó el Concilio Vaticano II?","Respuestas":{"a":"Juan Pablo II","b":"Benedicto XVI","c":"Juan XXIII"},"Respuesta":"c","Explicación": "El Concilio Vaticano II lo convocó el Papa Juan XXIII.","Km":2},
+      {"Pregunta":"¿Cómo se llama la mansión del magnate en la película Ciudadano Kane?","Respuestas":{"a":"Rosebud","b":"Villa Favorita","c":"Xanadú"},"Respuesta":"c","Explicación": "La mansión de Kane es Xanadú; Rosebud era el trineo con el que jugó en su infancia.","Km":1},
+      {"Pregunta":"¿Cómo muere don Quijote?","Respuestas":{"a":"Se cae del caballo cuando acomete al molino que confunde con un gigante","b":"En un enfrentamiento con bandoleros","c":"En la cama"},"Respuesta":"c","Explicación": "Alonso Quijano muere en la cama; confesado, cuerdo y no creyéndose don Quijote.","Km":1},
+      {"Pregunta":"Maximiliano I, emperador de México en la segunda intervención francesa, nació en...","Respuestas":{"a":"Viena","b":"Querétaro","c":"París"},"Respuesta":"a","Explicación": "Maximiliano de Habsburgo, archiduque de Austria, nació en Viena.","Km":1},
+      {"Pregunta":"En un ojo miope la imagen se forma...","Respuestas":{"a":"Por delante de la retina","b":"Por detrás de la retina","c":"En el humor vítreo"},"Respuesta":"a","Explicación": "En un ojo miope, las imágenes se forman delante de la retina.","Km":1},
+      {"Pregunta":"¿Por qué algunas frecuencias reciben el nombre de 'ultrasonidos'?","Respuestas":{"a":"Porque están situadas por encima del umbral auditivo","b":"Porque producen ondas especialmente amplias","c":"Porque se las oye desde muy lejos"},"Respuesta":"a","Explicación": "Los ultrasonidos se sitúan por encima del umbral de percepción.","Km":2},
+      {"Pregunta":"El Principito le pide a Saint-Exupéry que le dibuje...","Respuestas":{"a":"Un sombrero","b":"Un cordero","c":"Un elefante en el estómago de una boa"},"Respuesta":"b","Explicación": "¡Dibújame un cordero!, le pide El Principito al aviador.","Km":2},
+      {"Pregunta":"China es un régimen...","Respuestas":{"a":"Comunista","b":"Maoísta","c":"Capitalista"},"Respuesta":"a","Explicación": "El régimen político chino sigue siendo comunista a pesar de la apertura de su economía al capitalismo. Dejó de ser maoísta en 1978.","Km":3},
+      {"Pregunta":"¿Cuál de los siguientes no es un modo verbal?","Respuestas":{"a":"El indicativo","b":"El subjuntivo","c":"El imperfecto"},"Respuesta":"c","Explicación": "El imperfecto no es un modo verbal, sino un tiempo.","Km":2},
+      {"Pregunta":"¿Qué máxima es propia del racionalismo?","Respuestas":{"a":"Errare humanum est","b":"Cogito ergo sum","c":"Pienso que pienso, luego pienso que existo"},"Respuesta":"b","Explicación": "La locución latina 'cogito, ergo sum' (pienso, luego existo) es fundamental en el discurso cartesiano.","Km":1},
+      {"Pregunta":"¿Qué es el nodo sinoatrial?","Respuestas":{"a":"El marcapasos natural del corazón","b":"El punto de corte de dos curvas senoidales","c":"La unión de materia y antimateria"},"Respuesta":"a","Explicación": "Los peregrinos se pueden ganar el jubileo en Año Santo.","Km":1},
+      {"Pregunta":"¿Qué significa la 'K' de KFOR?","Respuestas":{"a":"Kilo","b":"Kosovo","c":"Kenia"},"Respuesta":"b","Explicación": "El nodo sinoatrial es el marcapasos natural del corazón.","Km":1}
+      ];
 
   //=======================================================================
 
@@ -260,7 +342,7 @@
         },
 
         getQuestion: function(){
-            var vQuestionNum =  Math.floor(Math.random() * 10)+1;
+            var vQuestionNum =  Math.floor(Math.random() * 50)+1;
             gMaraton.vQuestion=vPreguntas[vQuestionNum].Pregunta;
             gMaraton.vAnswerA=vPreguntas[vQuestionNum].Respuestas.a;
             gMaraton.vAnswerB=vPreguntas[vQuestionNum].Respuestas.b;
@@ -278,8 +360,8 @@
         },
     };
 
-    var vStartGeneralTime=30;
-    var vStartQuestionTime=5;
+    var vStartGeneralTime=5;
+    var vStartQuestionTime=7;
     var vPlayerProgress=0;
     var vIgnoranceProgress=0;
 
